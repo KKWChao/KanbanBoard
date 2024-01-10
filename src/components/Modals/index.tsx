a330adfa79df67018c132a21e6bc6956c0cd5d6d
Kelvin Chao <kelvinchao2011@gmail.com>
2024-01-09 23:25:01 -0800

    working on modal

--- src/components/Modals/index.tsx
+++ src/components/Modals/index.tsx
@@ -2,9 +2,9 @@ export const SuccessModal = () => {
   return (
     <aside
       role="alert"
-      className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 h-1/4 w-1/2 bg-blue-500 rounded"
+      className="absolute top-10 left-1/2 transform -translate-x-1/2 h-24 w-1/4 bg-blue-500 rounded bg-opacity-30 flex items-center justify-center"
     >
-      Success
+      <h3 className="">Success</h3>
     </aside>
   );
 };
@@ -13,7 +13,7 @@ export const FailureModal = () => {
   return (
     <aside
       role="alert"
-      className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 h-1/4 w-1/2 bg-red-500 rounded"
+      className="absolute left-1/2 transform -translate-x-1/2  h-1/4 w-1/2 bg-red-500 rounded"
     >
       Failure
     </aside>
